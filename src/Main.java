5d6a9f14a6770ab488191ec890967e4e22c79e27
Harpal <harpal.songara.sbg@gmail.com>
2023-10-14 22:24:04 +0530

    added somthing

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    @getmapping("/welcome")
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+       
+    }
+}
+/*echo "# github-actions" >> README.md
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin git@github.com:harpalsongara/github-actions.git
+        git push -u origin main*/
